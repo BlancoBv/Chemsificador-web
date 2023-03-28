e5e84b0b40add09b6f48d0b00c3df1dad71f3b2e
Blanco <dehara14@gmail.com>
2023-03-27 22:23:40 -0600

    cambios minimos

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Content() {
-  return <div style={{ minHeight: "80vh", overflowY: "scroll" }}>Content</div>;
+  return (
+    <div className="container-fluid mt-3" style={{ minHeight: "80vh" }}>
+      Content
+    </div>
+  );
 }
 
 export default Content;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 function Footer() {
   const date = new Date();
 
   return (
     <div className="sticky-bottom border-top" style={{ height: "10vh" }}>
-      Chemsificador {date.getFullYear()}
+      <p className="m-3">Chemsificador {date.getFullYear()}</p>
     </div>
   );
 }
