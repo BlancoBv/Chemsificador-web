26c2a1b22d1809a128e4c5608d16a351a92c8ca8
Blanco <dehara14@gmail.com>
2023-07-16 21:14:40 -0600

    añadido modo oscuro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import { Link, useNavigate } from "react-router-dom";
 import cheems from "../img/cheems.png";
+import { useEffect, useState } from "react";
 function Header() {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
   const navigate = useNavigate();
+  const changeTheme = (e) => {
+    const checked = e.target.checked;
+    if (checked) {
+      setTheme(true);
+    } else {
+      setTheme(false);
+    }
+  };
+  useEffect(() => {
+    localStorage.setItem("darkMode", theme);
+    if (theme) {
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", "white");
+    }
+  }, [theme]);
+
   return (
     <div className="d-flex justify-content-start align-items-center border-bottom w-100 shadow sticky-top p-3 h-10 position-relative">
       {" "}
@@ -17,14 +38,20 @@ function Header() {
       {/* Links */}
       <div className="about-button d-flex">
         <div className="d-flex align-items-center me-5">
-          <i class="fa-solid fa-sun me-2" />
+          <i className="fa-solid fa-sun me-2" />
           <div className="form-check  form-switch d-flex">
-            <input type="checkbox" className="form-check-input" role="switch" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              role="switch"
+              onChange={changeTheme}
+              checked={theme}
+            />
           </div>
-          <i class="fa-solid fa-moon" />
+          <i className="fa-solid fa-moon" />
         </div>
         <Link to="about">
-          <i class="fa-solid fa-circle-info fs-3" />
+          <i className="fa-solid fa-circle-info fs-3" />
         </Link>
       </div>
     </div>
